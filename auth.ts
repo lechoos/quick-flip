7ba8b6cf55aea793880e48f3ca67a85df278bba2
Piotr Lechowicz <ptrlechowicz@gmail.com>
2025-01-28 20:19:55 +0100

    chore: configured a database, Prisma ORM and Credentials Provider for next-auth

--- auth.ts
+++ auth.ts
@@ -1,5 +1,26 @@
 import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from './prisma';
+import Credentials from '@auth/core/providers/credentials';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Credentials({
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'your@email.com',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+        },
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/auth/login',
+  },
 });
