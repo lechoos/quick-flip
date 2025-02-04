eb0561bc0f2e8ca7bb7703f7a869d457d52348ca
Piotr Lechowicz <ptrlechowicz@gmail.com>
2025-02-04 18:38:36 +0100

    feat: created a basic flashcard design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,21 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Anonymous_Pro, Inter } from 'next/font/google';
 import './globals.scss';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+const anonymousPro = Anonymous_Pro({
+  variable: '--font-anonymous-pro',
+  subsets: ['latin-ext'],
+  weight: ['400', '700'],
 });
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const inter = Inter({
+  variable: '--font-inter',
   subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
   title: 'Quick Flip',
-  description: 'Your best app to learn and practice any skill you want.',
+  description: 'Your best app to learn and practice Spanish. | La mejor aplicación para aprender y practicar español.',
 };
 
 export default function RootLayout({
@@ -24,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${anonymousPro.variable} ${inter.variable} antialiased`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,45 @@
+import { Flashcard } from '@/components/ui/flashcard';
+
+type FlashcardProps = {
+  variant: 'primary' | 'secondary' | 'accent' | 'destructive';
+  word: string;
+  partOfSpeech: 'noun' | 'verb' | 'adjective';
+  translation: string;
+  example?: string;
+};
+
+const data: FlashcardProps[] = [
+  {
+    variant: 'primary',
+    word: 'tener',
+    partOfSpeech: 'verb',
+    translation: 'to have',
+    example: 'Yo tengo un amigo.',
+  },
+  {
+    variant: 'accent',
+    word: 'el vino',
+    partOfSpeech: 'noun',
+    translation: 'wine',
+  },
+  {
+    variant: 'secondary',
+    word: 'asqueroso',
+    partOfSpeech: 'adjective',
+    translation: 'ready',
+    example: '¿Están listos para pedir?',
+  },
+];
+
 export default function Home() {
-  return <h1>Quick Flip</h1>;
+  return (
+    <div className="flex gap-4">
+      {data.map((flashcard) => (
+        <Flashcard
+          key={flashcard.word}
+          {...flashcard}
+        />
+      ))}
+    </div>
+  );
 }
