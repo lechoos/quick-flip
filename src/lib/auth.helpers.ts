c4666e47dabcc68cf40b3cbf1263faf40005e8f2
Piotr Lechowicz <ptrlechowicz@gmail.com>
2025-02-09 21:01:16 +0100

    feat: created helper functions for authentication

---
+++ src/lib/auth.helpers.ts
@@ -0,0 +1,66 @@
+import { z } from 'zod';
+import { hash, compare } from 'bcrypt';
+import { prisma } from '@/../prisma';
+import { registerSchema } from '@/lib/formSchemas';
+
+const SALT_ROUNDS = 12;
+
+type User = z.infer<typeof registerSchema>;
+
+export const checkUserExist = async (email: string) => {
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  return !!existingUser;
+};
+
+export const createUser = async (data: User) => {
+  const hashedPassword = await hash(data.password, SALT_ROUNDS);
+
+  const user = await prisma.user.create({
+    data: {
+      email: data.email,
+      username: data.username,
+      password: hashedPassword,
+    },
+  });
+
+  const { email, username } = user;
+
+  return { email, username };
+};
+
+export const getUserData = async (userId: string) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  if (!user) return null;
+
+  const { email, username } = user;
+
+  return { email, username };
+};
+
+export const verifyUser = async (credentials: Pick<User, 'email' | 'password'>) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: credentials.email,
+    },
+  });
+
+  if (!user) return null;
+
+  const isPasswordValid = await compare(credentials.password, user.password);
+
+  if (!isPasswordValid) return null;
+
+  const { email, username } = user;
+
+  return { email, username };
+};
