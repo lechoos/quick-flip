192ddc718d5146f3757fac4b700ed23e4cab0805
Piotr Lechowicz <ptrlechowicz@gmail.com>
2025-02-11 20:03:35 +0100

    feat: updated an AuthLayout

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,13 +1,10 @@
 import { AuthLayout } from '@/components/AuthLayout';
 import { LoginForm } from '@/components/forms/LoginForm';
-import { Card } from '@/components/ui/card';
 
 export default function LoginPage() {
   return (
-    <AuthLayout>
-      <Card className="p-3 min-w-[500px] bg-secondary border-2 border-secondary-border shadow-secondary-border">
-        <LoginForm />
-      </Card>
+    <AuthLayout title="Login">
+      <LoginForm />
     </AuthLayout>
   );
 }
--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -1,14 +1,16 @@
 import { ReactNode } from 'react';
+import { Card } from '@/components/ui/card';
 
 type LayoutProps = {
   children: ReactNode;
+  title: string;
 };
 
-export const AuthLayout = ({ children }: LayoutProps) => {
+export const AuthLayout = ({ title, children }: LayoutProps) => {
   return (
     <section className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="mb-2">Login</h1>
-      {children}
+      <h1 className="mb-2">{title}</h1>
+      <Card className="p-3 min-w-[500px] bg-secondary border-2 border-secondary-border shadow-secondary-border">{children}</Card>
     </section>
   );
 };
