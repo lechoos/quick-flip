b22631b5bd543851fa5f743ccb7be4a7e1bd574e
Piotr Lechowicz <ptrlechowicz@gmail.com>
2025-03-15 15:26:04 +0100

    feat: updated the Carousel component and moved practice mode to a separate file

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -3,6 +3,7 @@ import { use } from 'react';
 import { SlidesContext } from '@/context/SlidesContext';
 import useEmblaCarousel from 'embla-carousel-react';
 import { PracticeMode } from '@/pages/Mode/ModeContent/PracticeMode';
+import { Link } from '@/components/atoms/link';
 
 type Props = {
   setIsFinished: Dispatch<SetStateAction<boolean>>;
@@ -21,19 +22,26 @@ export const Carousel = ({ setIsFinished }: Props) => {
     }
   };
 
-  if (slides) {
+  if (!slides) {
     return (
-      <>
-        <div
-          className="embla"
-          ref={emblaRef}
-        >
-          <div className="embla-container">{slides}</div>
-        </div>
-        <div className="mt-4 space-x-2">
-          <PracticeMode slideNext={slideNext} />
-        </div>
-      </>
+      <h2 className="flex flex-col gap-y-2 mt-2 text-center">
+        No flashcards found.
+        <Link href="/flashcards">Go back to flashcards page.</Link>
+      </h2>
     );
   }
+
+  return (
+    <>
+      <div
+        className="embla"
+        ref={emblaRef}
+      >
+        <div className="embla-container">{slides}</div>
+      </div>
+      <div className="mt-4 space-x-2">
+        <PracticeMode slideNext={slideNext} />
+      </div>
+    </>
+  );
 };
