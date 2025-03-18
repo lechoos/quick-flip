27227317d72d699db03e7f8ef067201b78da81ae
Piotr Lechowicz <ptrlechowicz@gmail.com>
2025-03-18 13:47:25 +0100

    feat: created a subtitle displaying info about user logged in or info that man can create a new account or login to existing one

--- src/app/flashcards/page.tsx
+++ src/app/flashcards/page.tsx
@@ -1,6 +1,7 @@
 import { getAllCategories } from '@/lib/actions';
 import { CategoryLabel } from '@/components/ui/CategoryLabel';
 import { AlbumIcon } from 'lucide-react';
+import { Subtitle } from '@/pages/flashcards/Subtitle';
 
 export const dynamic = 'force-dynamic';
 
@@ -20,6 +21,7 @@ export default async function FlashcardsPage() {
       <header className="relative py-4 pt-7 sm:pt-[13rem] bg-primary border-[5px] border-black">
         <h1 className="text-center">Pick one of these categories:</h1>
       </header>
+      <Subtitle />
       <div className="flex flex-wrap justify-center gap-x-2 gap-y-4 mt-2 mx-2">
         {categories.map((category) => (
           <CategoryLabel
---
+++ src/pages/flashcards/Subtitle.tsx
@@ -0,0 +1,37 @@
+import { getUserProfile } from '@/lib/actions';
+import { Link } from '@/components/atoms/link';
+import { SignOutButton } from '@/components/ui/SignOutButton';
+
+export const Subtitle = async () => {
+  const user = await getUserProfile();
+
+  const commonStyles = 'mx-4 mt-2 text-lg text-center';
+
+  if (!user) {
+    return (
+      <h2 className={commonStyles}>
+        You can also{' '}
+        <Link
+          className="underline"
+          href={'/auth/register'}
+        >
+          Register
+        </Link>{' '}
+        and{' '}
+        <Link
+          className="underline"
+          href={'/auth/login'}
+        >
+          Login
+        </Link>{' '}
+        but now it doesn't provide any features yet.
+      </h2>
+    );
+  }
+
+  return (
+    <h2 className={commonStyles}>
+      You are logged in as {user.username}. <SignOutButton />
+    </h2>
+  );
+};
