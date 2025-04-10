3d0c237629e46c237f721fa4c030ec6abfe3638e
Piotr Lechowicz <ptrlechowicz@gmail.com>
2025-04-10 18:46:44 +0200

    feat: updated the navigation

--- src/components/ui/Navigation.tsx
+++ src/components/ui/Navigation.tsx
@@ -8,8 +8,12 @@ import { AlignJustify, Plus } from 'lucide-react';
 
 const links = [
   {
-    label: 'Demo',
+    label: 'Cards',
     href: '/flashcards',
+  },
+  {
+    label: 'Profile',
+    href: '/profile',
   },
 ];
 
@@ -18,23 +22,21 @@ type Props = {
   setIsOpen: () => void;
 };
 
-const Button = ({ isOpen, setIsOpen }: Props) => {
-  return (
-    <button
-      onClick={() => setIsOpen()}
-      className="absolute top-0 left-0 inline-block sm:hidden p-2 z-50"
-    >
-      {isOpen ? (
-        <Plus
-          className="rotate-45"
-          size={40}
-        />
-      ) : (
-        <AlignJustify size={40} />
-      )}
-    </button>
-  );
-};
+const Button = ({ isOpen, setIsOpen }: Props) => (
+  <button
+    onClick={() => setIsOpen()}
+    className="absolute top-0 left-0 inline-block sm:hidden p-2 z-50"
+  >
+    {isOpen ? (
+      <Plus
+        className="rotate-45"
+        size={40}
+      />
+    ) : (
+      <AlignJustify size={40} />
+    )}
+  </button>
+);
 
 export const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -52,11 +54,14 @@ export const Navigation = () => {
         isOpen={isOpen}
         setIsOpen={() => setIsOpen((prev) => !prev)}
       />
-      <nav className={`absolute top-8 sm:top-2 ${!isOpen ? 'right-full' : 'right-1/2 translate-x-1/2'} sm:right-1/2 sm:translate-x-1/2 flex justify-between sm:block mx-1 py-[2.5rem] px-2 sm:px-4 w-[80%] max-w-[350px] font-anonymous-pro font-bold text-xl bg-secondary border-[5px] border-black shadow rounded-[60px] z-20`}>
-        <Link href="/">
+      <nav className={`absolute top-8 sm:top-2 ${!isOpen ? 'right-full' : 'right-1/2 translate-x-1/2'} sm:right-1/2 sm:translate-x-1/2 flex flex-col justify-between sm:block mx-1 py-[2.5rem] px-2 sm:px-4 w-[80%] max-w-[350px] font-anonymous-pro font-bold text-xl bg-secondary border-[5px] border-black shadow rounded-[60px] z-20`}>
+        <Link
+          className="flex justify-center"
+          href="/"
+        >
           <QuickLogo className="sm:absolute left-4 top-1/2 sm:-translate-y-1/2" />
         </Link>
-        <div className="flex gap-x-[1.2rem] justify-end sm:items-center mt-1 sm:mt-0 sm:ml-2">
+        <div className="flex gap-x-[1.2rem] justify-center sm:justify-end sm:items-center mt-2 sm:mt-0 sm:ml-2">
           {links.map((link) => (
             <Link
               key={link.label}
