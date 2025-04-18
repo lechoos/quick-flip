ef65d9b647f8ff6e618aec9eff987f27f35e3367
Piotr Lechowicz <ptrlechowicz@gmail.com>
2025-04-18 14:37:12 +0200

    feat: updated register schema to display a proper message when required field is empty

--- src/lib/formSchemas.ts
+++ src/lib/formSchemas.ts
@@ -17,10 +17,23 @@ export const loginSchema = z.object({
 export type LoginSchemaType = typeof loginSchema;
 
 export const registerSchema = z.object({
-  email: z.string().min(1, 'Email is required').email('Invalid email format'),
-  password: z.string().min(8, 'Password must be at least 8 characters long').regex(/[A-Z]/, 'Password must contain at least one uppercase letter').regex(/[0-9]/, 'Password must contain at least one number'),
+  email: z
+    .string({
+      required_error: 'Email is required',
+    })
+    .min(1, 'Email is required')
+    .email('Invalid email format'),
+  password: z
+    .string({
+      required_error: 'Password is required',
+    })
+    .min(8, 'Password must be at least 8 characters long')
+    .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+    .regex(/[0-9]/, 'Password must contain at least one number'),
   username: z
-    .string()
+    .string({
+      required_error: 'Username is required',
+    })
     .min(3, 'Username must be at least 3 characters long')
     .max(30, 'Username must not exceed 30 characters')
     .regex(/^[a-zA-Z0-9_-]+$/, 'Username can only contain letters, numbers, underscores and dashes'),
