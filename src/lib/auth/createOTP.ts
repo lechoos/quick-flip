7d2ce8c034aa6155770fc50b48b5511c37560059
Piotr Lechowicz <ptrlechowicz@gmail.com>
2025-05-03 20:22:25 +0200

    fix: fixed a linter error

--- src/lib/auth/createOTP.ts
+++ src/lib/auth/createOTP.ts
@@ -1,7 +1,7 @@
 import { hash } from 'bcryptjs';
 
 export const createOTP = async () => {
-  const digits = Array.from({ length: 6 }).map((_) => Math.round(Math.random() * 9));
+  const digits = Array.from({ length: 6 }).map(() => Math.round(Math.random() * 9));
 
   const otp = digits.reduce((curr, acc) => String(curr) + String(acc), '');
   const hashedOTP = await hash(otp, 12);
